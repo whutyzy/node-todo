79954c0c856d1b5716e67cdd8eba11e3900c1e45
whutyzy <yeziyang@qiyi.com>
2019-10-26 16:16:18 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,10 @@
+const program = require('commander');
+
+program
+    .option('-d, --debug', 'output extra debugging')
+    .option('-s, --small', 'small pizza size')
+    .option('-p, --pizza-type <type>', 'flavour of pizza');
+
+program.parse(process.argv);
+
+console.log()
